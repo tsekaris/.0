3f092b44dae1a0a6ea1b50c256fbe607c9f4111d
tsekaris <tsemix@gmail.com>
2018-12-22 16:05:32 +0200

    vim python

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ pkg install git
 pkg install stow
 pkg install ranger
 
-pkg install vim
+pkg install vim vim-python
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 stow --target=$HOME vim
 vim +PluginInstall +qall
