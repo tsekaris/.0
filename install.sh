79c8589460a2ad3ae3b819689487e2db907b1d1a
tsekaris <tsemix@gmail.com>
2018-12-22 16:00:18 +0200

    add vim with dotfile

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 
 pkg upgrade
 termux-setup-storage
+pkg install git
+pkg install stow
 pkg install ranger
-pkg install vim
 
+pkg install vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+stow --target=$HOME vim
+vim +PluginInstall +qall
