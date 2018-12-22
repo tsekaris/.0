af6f2962b8dfd65e2a3e515322fa00930c26da14
tsekaris <tsemix@gmail.com>
2018-12-22 16:30:31 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,10 +7,10 @@ pkg install stow
 pkg install ranger
 pkg install curl
 
-pkg install zsh
-bash -c "$(curl -fsSL https://git.io/oh-my-termux)"
-
 pkg install vim vim-python
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 stow --target=$HOME vim
 vim +PluginInstall +qall
+
+pkg install zsh
+bash -c "$(curl -fsSL https://git.io/oh-my-termux)"
