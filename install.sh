4856c6d4e58632c41e390bbfef39a2925d1cec10
tsekaris <tsemix@gmail.com>
2018-12-22 16:08:54 +0200

    zsh

--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@ termux-setup-storage
 pkg install git
 pkg install stow
 pkg install ranger
+pkg install curl
+
+pkg install zsh
+bash -c "$(curl -fsSL https://git.io/oh-my-termux)"
 
 pkg install vim vim-python
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
