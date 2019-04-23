af147b8e98807e4bc6b18c69e792f45db86f47ee
tsekaris <tsemix@gmail.com>
2019-04-23 12:28:19 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,42 @@
 #!/bin/sh
 
+
 pkg upgrade
-termux-setup-storage
-pkg install git
-pkg install stow
-pkg install ranger
-pkg install curl
-pkg install nodejs
+termux-setup-storage # Για να βλέπω τους φακέλους του κινητού
 
-pkg install vim-python # Για να μπορώ να εγκαταστήσω pluguns
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-stow --target=$HOME vim
-vim +PluginInstall +qall
+# system.
+pkg install git -y
+pkg install stow -y
+pkg install curl -y
+pkg install tmux -y
+pkg install htop -y
 
-#pkg install zsh
+# ssh server
+pkg install openssh -y
+passwd # Για να ορίσω κωδικό. Από termux.
+
+# vpn
+pkg install wireguard-tools -y
+
+# data
+pkg install syncthing -y
+
+# File manager.
+pkg install ranger -y
+pkg install nnn -y
+
+# Text editor.
+pkg install vim-python # Για να μπορώ να εγκαταστήσω plugins
+
+# Coding.
+pkg install nodejs -y
+# npm install -g --unsafe-perm node-red # Βγάζει λάθη
+
+# Server and mqtt.
+pkg install mosquitto -y
+
+## Media.
+pkg install mpv -y # Για radio.
+
+#pkg install zsh -y
 #bash -c "$(curl -fsSL https://git.io/oh-my-termux)"
