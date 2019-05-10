5e9f25c88f00a7444ddad51d9b851527d622a99d
tsekaris <tsemix@gmail.com>
2019-05-10 11:45:48 +0300

    Update .install.sh

--- termux/.install.sh
+++ termux/.install.sh
@@ -2,5 +2,6 @@
 
 pkg upgrade -y
 termux-setup-storage # Για να βλέπω τους φακέλους του κινητού
-ln -s $HOME/storage/shared/0 $HOME/0 # Συντόμευση του 0 στο home 
-stow --target=$HOME $HOME/.0/termux/.dotfiles
+ln -s $HOME/storage/shared/0 $HOME/0 # Συντόμευση του 0 στο home
+cd $HOME/.0/termux
+stow --target=$HOME .dotfiles
