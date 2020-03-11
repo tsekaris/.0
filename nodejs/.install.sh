ad9a4aead68e607b52fe147ba295f09cc58f1903
tsekaris <tsemix@gmail.com>
2020-03-11 12:18:49 +0200

    Update node install like ubuntu

--- nodejs/.install.sh
+++ nodejs/.install.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 pkg install nodejs-lts -y
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+#line="export PATH=$PATH:~/.npm-global/bin"
+#grep -Fxq "$line" ~/.bashrc || echo "$line" >> ~/.bashrc
+export PATH=$PATH:~/.npm-global/bin # Για να έχει συνέχεια τα installs χωρίς "source .bashrc"
+npm install -g npm
