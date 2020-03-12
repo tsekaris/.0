397585c2655192d2f2bdcea40b4f39c5f18836a8
tsekaris <tsemix@gmail.com>
2020-03-12 07:35:48 +0200

    changes

--- nodejs/create_project.sh
+++ nodejs/create_project.sh
@@ -2,7 +2,8 @@
 
 touch index.js
 npm init --yes # This will trigger automatically populated initialization.dd
-npm i -D eslint prettier eslint-plugin-prettier eslint-config-prettier
+npm i -D eslint prettier eslint-plugin-prettier eslint-config-prettier typescript
+# typescript: tsserver για ale auto completion.
 npx install-peerdeps --dev eslint-config-airbnb-base #Στην ουσία install eslint-config-airbnb-base (χωρίς react) eslint-plugin-import
 cp $HOME/.0/nodejs/.eslintrc.js .
 cp $HOME/.0/nodejs/.prettierrc.js .
