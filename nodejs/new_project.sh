3fb8b0c48d6f8407fecb13fc7d49131ff02df751
tsekaris <tsemix@gmail.com>
2020-03-13 16:58:17 +0200

    Update new_project.sh

--- nodejs/new_project.sh
+++ nodejs/new_project.sh
@@ -3,5 +3,5 @@
 touch index.js
 npm init 
 npm install -D eslint #Αν εγκαταστήσω global ο wizard θα το  εγκαταστήσει και local.
-npm install -D typescript  #Αν εγκαταστήσω global ο wizard θα το  εγκαταστήσει και local.
+npm install -D typescript  #Για το autocompletion του ale.
 npx eslint --init
