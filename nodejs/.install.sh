e8fa12d60e7354a949f09754261a70ba5dc359f1
tsekaris <tsemix@gmail.com>
2020-03-13 09:47:34 +0200

    node

--- nodejs/.install.sh
+++ nodejs/.install.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
 pkg install nodejs-lts -y
-mkdir ~/.npm-global
-npm config set prefix '~/.npm-global'
-#line="export PATH=$PATH:~/.npm-global/bin"
-#grep -Fxq "$line" ~/.bashrc || echo "$line" >> ~/.bashrc
-export PATH=$PATH:~/.npm-global/bin # Για να έχει συνέχεια τα installs χωρίς "source .bashrc"
-npm install -g npm
+
+f(){
+  mkdir ~/.npm-global
+  npm config set prefix '~/.npm-global'
+  #line="export PATH=$PATH:~/.npm-global/bin"
+  #grep -Fxq "$line" ~/.bashrc || echo "$line" >> ~/.bashrc
+  export PATH=$PATH:~/.npm-global/bin # Για να έχει συνέχεια τα installs χωρίς "source .bashrc"
+  npm install -g npm
+}
+
+# Δεν χρειάζεται
+#f
