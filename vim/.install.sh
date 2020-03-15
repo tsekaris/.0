89903c7fa69c7f2a9baebab16cc84dc7e1ed6258
tsekaris <tsemix@gmail.com>
2020-03-15 21:08:11 +0200

    Update .install.sh

--- vim/.install.sh
+++ vim/.install.sh
@@ -3,8 +3,7 @@
 # pkg install vim-python -y # Για να δουλεύει με τα plugins που εξαρτιόνται από python. Βγάζει θέματα σε updates.
 pkg install vim -y
 pkg install vim-python -y #Κάνει uninstall το vim. Κατά περιόδους κρασάρει σε install ή update.
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cd $HOME/.0/vim
 stow --target=$HOME .dotfiles
-#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall
