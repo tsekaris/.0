7fb0995434daf08ff9769ede79c6b223e2e4f3cf
tsekaris <tsemix@gmail.com>
2020-03-21 19:41:29 +0200

    ...

--- rclone/gdrive.sh
+++ rclone/gdrive.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo 
 echo "rclone syncing to gdrive..."
-rclone sync --progress ~/0 gdrive:0 --exclude "/engineering/**" 
+rclone sync --verbose --progress ~/0 gdrive:0 --exclude "/engineering/**" 
 echo "rclone finished. Press enter."
 echo 
 read pause
