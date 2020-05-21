d25da474005fd4e52e1d8b99124adc969fd93513
tsekaris <tsemix@gmail.com>
2020-05-21 15:28:44 +0300

    node changes

--- nodejs/new_project.sh
+++ nodejs/new_project.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
 
+
+git init
 touch index.js
-npm init 
+npx license MIT #LICENSE file
+npx gitignore node #gitignore file
+
+# Τα παρακάτω είναι για τα defaults του "npm init -y"
+npm set init.author.name "Tsekaris Michael"
+npm set init.author.email "tsemix@gmail.com"
+npm set init.author.url "https://github.com/tsekaris/"
+npm set init.license "MIT"
+npm set init.version "0.0.0"
+npm init -y 
+
 npm install -D eslint #Αν εγκαταστήσω global ο wizard θα το  εγκαταστήσει και local.
-npm install -D typescript  #Για το autocompletion του ale.
+npm install -D typescript  #Για το autocompletion.
 npx eslint --init
+
+git add -A
+git commit -m "Initial commit."
