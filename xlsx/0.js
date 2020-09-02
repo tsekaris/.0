09386fc4f581061e4438189176a75e0542bcdf6b
tsekaris <tsemix@gmail.com>
2020-09-02 18:10:49 +0300

    ...

--- xlsx/0.js
+++ xlsx/0.js
@@ -1,20 +1,25 @@
 const xlsx = require('xlsx');
 const fetch = require('node-fetch');
 const fsp = require('fs').promises;
+const inquirer = require('inquirer');
 
 (async () => {
-  const link = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTKbEMf86Olz7cmKpviIAyuvGNisEVH5QPCv3AVNe8N8p1jwmPSpmc0B-5r6eiCpB9PMWsR6lpZE1-2/pub?output=xlsx';
+  const link = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSIltzx-zQxBvDdgkBaP8Jbl62hUheFKy0NnkswMyG4Pl1HFxJfr1LXD3uRitr06OqucT5_TG34Yqfr/pub?output=xlsx';
   const response = await fetch(link);
   const buffer = await response.buffer();
   const workbook = xlsx.read(buffer, { type: 'buffer' });
-  const sheetNames = workbook.SheetNames;
-  const data = {};
+  const answer = await inquirer.prompt({
+    type: 'checkbox',
+    name: 'sheets',
+    message: 'Select sheets:',
+    choices: workbook.SheetNames,
+  });
+  let data = [];
 
-  sheetNames.forEach((sheetName) => {
-    data[sheetName] = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
+  answer.sheets.forEach((sheetName) => {
+    data = [...data, ...xlsx.utils.sheet_to_json(workbook.Sheets[sheetName])];
   });
   await fsp.mkdir('.tmp', { recursive: true }); // Απαραίτητο το recursive στην περίπτωση που υπάρχει ο φάκελος
   await fsp.writeFile('.tmp/data.json', JSON.stringify(data, null, 2));
-
-  console.dir(data);
-})();
+  return data;
+})().then(console.dir);
