55a558228016511524187d9d50858224be57bf48
tsekaris <tsemix@gmail.com>
2020-09-04 11:47:43 +0300

    Update 0.js
    ...

--- pouchdb/0.js
+++ pouchdb/0.js
@@ -8,3 +8,23 @@ db.sync(dbServer, {
 }).on('error', function(err) {
     console.log(err);
 });
+
+let a = 14;
+
+querry = {
+    selector: {
+        type: 'mcb',
+        p: '1p',
+        ch: 'B',
+        kA: 10,
+        aMin: {
+            $gte: a
+        }
+    },
+    fields: ['_id'],
+    limit: 1
+}
+
+
+db.find(querry).then(console.log);
+
