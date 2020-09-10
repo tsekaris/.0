9beb154b301d5156589b14e998395861c1f06232
tsekaris <tsemix@gmail.com>
2020-09-10 11:43:08 +0300

    ...

--- pouchdb_server/import.pc.js
+++ pouchdb_server/import.pc.js
@@ -1,23 +1,39 @@
-const fs = require('fs').promises;
+const xlsx = require('xlsx');
+const fetch = require('node-fetch');
+const inquirer = require('inquirer');
 const PouchDB = require('pouchdb');
 
-async function loadJSON() {
+const link = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQJSXIfxc37QtcXd_3Q4P7ZKHxExqj3A8g0Dh8n-q-aFrGlgCJAyPOAvFcTQvHpTpfndPnwKy-xUtrd/pub?output=xlsx';
+
+async function parts() {
   try {
     const dbOld = new PouchDB('http://localhost:3000/parts');
-    await dbOld.destroy();
+    await dbOld.destroy(); // delete και ξαναδημιουργία
     const db = new PouchDB('http://localhost:3000/parts');
-    const json = await fs.readFile(`${process.env.HOME}/.0/xlsx/.tmp/data.json`);
-    const result = await db.bulkDocs(JSON.parse(json));
-    result.forEach((item) => {
-      if (!item.ok) {
-        console.log(item.id);
-      }
+    const response = await fetch(link);
+    const buffer = await response.buffer();
+    const workbook = xlsx.read(buffer, { type: 'buffer' });
+    const answer = await inquirer.prompt({
+      type: 'checkbox',
+      name: 'sheets',
+      message: 'Select sheets:',
+      choices: workbook.SheetNames,
     });
-    return result;
+    let data = [];
+    answer.sheets.forEach((sheetName) => {
+      data = [...data, ...xlsx.utils.sheet_to_json(workbook.Sheets[sheetName])];
+    });
+    console.log(data.length);
+    return await db.bulkDocs(data);
   } catch (err) {
     console.log(err);
   }
 }
 
-// loadJSON().then(console.log);
-loadJSON();
+parts().then((result) => {
+  result.forEach((item) => {
+    if (!item.ok) {
+      console.log(item.id);
+    }
+  });
+});
