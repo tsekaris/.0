125d769365f5aa97c10b76e193a501c60e62c380
tsekaris <tsemix@gmail.com>
2020-09-13 17:47:44 +0300

    Update 0.js
    ...

--- pouchdb/0.js
+++ pouchdb/0.js
@@ -1,3 +1,4 @@
+/*
 var dbServer = new PouchDB('http://192.168.1.4:3000/parts');
 var db = new PouchDB('db');
 db.destroy().then(function() {
@@ -12,47 +13,83 @@ db.destroy().then(function() {
     
 
 })
+*/
+var db = new PouchDB('parts');
 
-async function abb(query={}) {
+//let link = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSIltzx-zQxBvDdgkBaP8Jbl62hUheFKy0NnkswMyG4Pl1HFxJfr1LXD3uRitr06OqucT5_TG34Yqfr/pub?output=tsv';
+//let link = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSIltzx-zQxBvDdgkBaP8Jbl62hUheFKy0NnkswMyG4Pl1HFxJfr1LXD3uRitr06OqucT5_TG34Yqfr/pub?output=csv';
+//let link = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSIltzx-zQxBvDdgkBaP8Jbl62hUheFKy0NnkswMyG4Pl1HFxJfr1LXD3uRitr06OqucT5_TG34Yqfr/pubhtml';
+
+
+/*
+fetch(link, {
+    mode: 'no-cors'
+}).then(response => response.text()).then(data => console.log(data));
+
+*/
+async function getFile(link){
+    let response = await fetch(link, {mode: 'no-cors'});
+    let text = await response.text();
+    return text;
+}
+
+
+//getFile(link).then(console.log).catch(console.log);
+
+async function abbIndex() {
     try {
-        let createIndex = await db.createIndex({
+        return await db.createIndex({
             index: {
                 fields: ['brand', 'type', 'p', 'a'],
                 name: 'abb',
                 ddoc: 'indexes'
             }
         });
-        if (createIndex.result === 'created' || createIndex.result === 'exists') {
-            console.log(createIndex);
-            query.brand = 'abb';
-            if (query.type === undefined) {
-                query.type = {
-                    $gt: null
-                }
-            }
-
-            if (query.p === undefined) {
-                query.p = {
-                    $gt: null
-                }
-            }
-
-            //απαραίτητο αν δεν ορίσω καμία μια μεταβλητή από το index. Με null δεν λειτουργεί
-            if (query.a === undefined) {
-                query.a = {
-                    $gt: 0
-                }
-            }
-
-            let result = await db.find({
-                selector: query,
-                fields: ['_id'],
-                use_index: ['indexes', 'abb'],
-            });
-            console.log(result);
-            return result;
-        }
     } catch (err) {
         console.log(err);
+    }
+}
+
+abbIndex().then(console.log);
+
+async function abb(query={}) {
+    try {
+        query.brand = 'abb';
+        if (query.type === undefined) {
+            query.type = {
+                $gt: null
+            }
+        }
+
+        if (query.p === undefined) {
+            query.p = {
+                $gt: null
+            }
+        }
+
+        //απαραίτητο αν δεν ορίσω καμία μια μεταβλητή από το index. Με null δεν λειτουργεί
+        if (query.a === undefined) {
+            query.a = {
+                $gt: 0
+            }
+        }
+
+        let result = await db.find({
+            selector: query,
+            fields: ['_id'],
+            use_index: ['indexes', 'abb'],
+        });
+        console.log(result);
+        return result;
+    } catch (err) {
+        console.log(err);
+        if (err.error === 'no_usable_index') {
+            abbIndex().then(result=>{
+                console.log(result);
+                abb(query);
+            }
+            );
+        }
+
     }
 }
