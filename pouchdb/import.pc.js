90208b677c2ea670b762a095a4dcc1eb88989635
tsekaris <tsemix@gmail.com>
2020-09-14 19:32:20 +0300

    ...

--- pouchdb/import.pc.js
+++ pouchdb/import.pc.js
@@ -1,11 +1,17 @@
 const fetch = require('node-fetch');
+const fs = require('fs');
 
 const link = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSIltzx-zQxBvDdgkBaP8Jbl62hUheFKy0NnkswMyG4Pl1HFxJfr1LXD3uRitr06OqucT5_TG34Yqfr/pub?output=tsv';
 async function parts() {
   try {
     const response = await fetch(link);
     const tsv = await response.text();
-    return tsv;
+    // const lines = tsv.split(/(?:\r\n|\r|\n)/g);
+    const lines = tsv.split('\n');
+    let data = [];
+    lines.forEach((line) => (data = [...data, ...JSON.parse(line)]));
+    fs.writeFileSync('parts.json', JSON.stringify(data, null, 2));
+    return data;
   } catch (err) {
     console.log(err);
   }
