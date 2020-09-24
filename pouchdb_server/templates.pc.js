3c40342a34d883383c488ba3cb4385f45d717d38
tsekaris <tsemix@gmail.com>
2020-09-24 16:07:06 +0300

    ...

---
+++ pouchdb_server/templates.pc.js
@@ -0,0 +1,79 @@
+const child_process = require('child_process');
+const colors = require('colors');
+
+function fzf(db) {
+  // db = {message: 'text', choices: []}
+
+  let script;
+  let data = [];
+  let returnObj = {};
+  function sh(req) {
+    const child = child_process.spawnSync(req, {
+      stdio: ['inherit', 'pipe', 'inherit'],
+      shell: true,
+      encoding: 'utf-8',
+    });
+    // slice: remove change line που προσθέτει στο stdout
+    const out = child.stdout.slice(0, -1);
+    let ok = false;
+    if (child.status === 0) {
+      ok = true;
+    }
+
+    return { ok, out };
+  }
+
+  if (db.choices !== undefined) {
+    script = sh(`echo "${db.choices.join('\n')}" | fzf --prompt "${db.message} " --marker="+" -m`);
+    if (script.ok) {
+      // Έχει επιλεχτεί κάτι
+      data = script.out.split('\n');
+    } else {
+      data = [];
+    }
+  } else {
+    script = sh(
+      `echo "" | fzf --color=bg+:-1 --info=hidden --pointer=' ' --print-query --prompt "${db.message} "`,
+    );
+    data = script.out.split('\n');
+    if (script.ok) {
+      // Έχει επιλεχτεί το τίποτα
+      data = [];
+    } else {
+      // Δεν έχει επιλεχτεί τίποτα άρα πρέπει να δούμε το text που εισάγεται (query)
+      const input = data[0];
+      data = [];
+      if (input !== '') {
+        data[0] = input;
+      }
+    }
+  }
+  if (data.length === 1) {
+    console.log(db.message.cyan, data[0].green);
+    returnObj = {
+      value: data[0],
+      values: data,
+      id: data[0].split(' ')[0],
+      ids: data.map((text) => text.split(' ')[0]),
+    };
+  } else if (data.length === 0) {
+    console.log(db.message.red.underline);
+    returnObj = {
+      value: null,
+      values: null,
+      id: null,
+      ids: null,
+    };
+  } else {
+    console.log(db.message.cyan);
+    data.forEach((element) => console.log(element.green));
+    returnObj = {
+      value: data[0],
+      values: data,
+      id: data[0].split(' ')[0],
+      ids: data.map((text) => text.split(' ')[0]),
+    };
+  }
+  return returnObj;
+}
+exports.fzf = fzf;
