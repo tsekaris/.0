9cb1518b2db85cfe73a03ba22c06fd42342080e9
tsekaris <tsemix@gmail.com>
2021-01-16 23:52:16 +0200

    ...

--- inquirer/testing.js
+++ inquirer/testing.js
@@ -3,9 +3,16 @@ const inquirer = require('inquirer');
 inquirer
   .prompt([
     {
-      type: 'editor',
+      type: 'input',
+      choices: ['paok', 'aris', 'ole'],
       name: 'story',
       message: 'Tell me a story, a really long one!',
+      validate(input) {
+        if (input > 10) {
+          return true;
+        }
+        return '> 10';
+      },
     },
   ])
   .then((answers) => {
